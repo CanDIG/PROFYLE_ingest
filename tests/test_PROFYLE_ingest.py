ddb598f3a1e9d9c1e5b220d67cf9f9fe48fcd96c
Jonathan Dursi <jonathan@dursi.ca>
2017-08-08 19:17:18 -0400

    Correct import, make sure import is used

--- tests/test_PROFYLE_ingest.py
+++ tests/test_PROFYLE_ingest.py
@@ -5,18 +5,20 @@
 
 
 import unittest
-
-from PROFYLE_ingest import PROFYLE_ingest
-
+from PROFYLE_ingest import create_repo
 
 class TestProfyle_ingest(unittest.TestCase):
     """Tests for `PROFYLE_ingest` package."""
 
     def setUp(self):
         """Set up test fixtures, if any."""
+        # some nonsese to make sure the profile gets used:
+        self._attrs = create_repo.AttributesList()
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
+        self._attrs = None
 
     def test_000_something(self):
         """Test something."""
+        pass
